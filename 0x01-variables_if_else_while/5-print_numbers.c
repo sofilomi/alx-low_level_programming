0ac20ade9b09467065d8376e88b60ff1086dccc1
sofilomi <sofilomi7@gmail.com>
2022-03-13 16:06:58 -0700

    yes

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - zero to nine
+ *
+ * Return: always (0)
+ *
+ */
+int main(void)
+{
+	int a = 0;
+
+	for (; a <= 9 ; a++)
+	{
+		printf("%i", a);
+	}
+	printf("\n");
+
+	return (0);
+}
