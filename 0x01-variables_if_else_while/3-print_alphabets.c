a2f2b8f0c033bff4a04f13b0c2ee82c713847ee6
sofilomi <sofilomi7@gmail.com>
2022-03-13 14:56:26 -0700

    yes

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main -entry comment 
+ *
+ * Return: always (0)
+ *
+ */
+int main(void)
+{
+	char sm = 'a';
+	char CA = 'A';
+	for (; sm <= 'z' ; sm++)
+	{
+		putchar(sm);
+	}
+	for (; CA <= 'Z' ; CA++)
+	{
+		putchar(CA);
+	}
+	putchar('\n');
+		return (0);
+}
