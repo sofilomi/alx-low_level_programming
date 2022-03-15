be9f9b966d07ec6de1a788c188eac51636a3d3f5
sofilomi <sofilomi7@gmail.com>
2022-03-15 14:14:03 -0700

    yes

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - ten times list of alphabet
+ *
+ * Return: always 0
+ */
+void print_alphabet_x10(void)
+{
+	
+	char ch;
+	int x = 0;
+
+	while (x < 10)
+	{
+		ch = 'a';
+
+		while (ch <= 'z')
+		{
+			_putchar(ch);
+			ch++;
+		}
+		_putchar('\n');
+		x++;
+
+	}
+	}
