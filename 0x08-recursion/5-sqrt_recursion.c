fcd0e67a7f3a3f4b5cf0e30229ada8055a18c9cb
sofilomi <sofilomi7@gmail.com>
2022-03-31 14:00:33 -0700

    recurr

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+/**
+ * _sqrt - function
+ * @n: the number
+ * @i: the number
+ * Return: Always 0
+ */
+
+int _sqrt(int n, int i)
+{
+	if (i * i >  n)
+		return (-1);
+	if (i * i == n)
+		return (i);
+	else
+		return (_sqrt(n, i + 1));
+}
+
+/**
+* _sqrt_recursion - function
+*
+* @n: the number
+* Return: Always 0.
+*/
+
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	if (n == 0 || n == 1)
+		return (n);
+	return (_sqrt(n, 0));
+}
